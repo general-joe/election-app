417adae63d7d2302d2c88067902fc23e0e06304d
Joe <kwabijoe274@gmail.com>
2023-06-19 22:06:03 +0000

    Imported node modules in the server

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const bodyparser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 8080;
+app.use(express.json());
+const appRoutes = require("./routes/index");
+app.use(bodyparser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", appRoutes);
+
 app.get("/", (req, res, next) => {
   res.status(200).json({
     message: "welcome to our  voting app",
   });
 });
-
 app.listen(PORT, () => {
-  console.log(`Server running on port${PORT}`);
+  console.log(`server running on port${PORT}`);
 });
